ac13749f9db04f69408d4b4664ee498ebfcfe3be
danielbasedow <daniel.basedow@gmail.com>
2018-03-09 18:58:08 +0100

    Initial Commit

---
+++ BLE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BLE
+//
+//  Created by Daniel Basedow on 09.03.18.
+//  Copyright © 2018 Daniel Basedow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BLE/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BLE
+//
+//  Created by Daniel Basedow on 09.03.18.
+//  Copyright © 2018 Daniel Basedow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
